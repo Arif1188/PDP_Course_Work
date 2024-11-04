df1c9548a70a981096f9a093a10fdcb5dbc4cc85
Arif Nabi <arifnabi571@gmail.com>
2024-11-04 17:48:03 +0500

    added word guessing game on python

---
+++ word_guessing.py
@@ -0,0 +1,63 @@
+import random
+
+name = str(input("What's your name:  "))
+print(f"Good Luck! {name}")
+word = 'geeks'
+asa = ''
+while asa !=word:
+    character = input("Guess a character: ")
+    print(asa)
+    for i in word:
+        if character == i:
+            asa+=character
+        if i in asa:
+            print(i)
+        else:
+            print("_")
+
+
+
+
+
+print(asa)
+
+
+# def is_fibonacci(n):
+#     if n < 0:
+#         return False
+    
+#     a, b = 0, 1
+#     while a < n:
+#         a, b = b, a + b
+    
+#     return a == n
+
+# # Example usage
+# numbers = [i for i in range(100)]
+# for number in numbers:
+#     if is_fibonacci(number):
+#         print(f"{number} is a Fibonacci number.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+        
