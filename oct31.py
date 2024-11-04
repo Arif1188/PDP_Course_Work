ad9548a210bde66dbb56d73050a22e85bd8cb1e3
Arif Nabi <arifnabi571@gmail.com>
2024-11-04 17:14:38 +0500

    added binary search logic

--- oct31.py
+++ oct31.py
@@ -33,21 +33,31 @@
 #     print(s)
     
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) +1):
-        print(i, 'is i')
-        print(n, 'is n')
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, int(n**0.5) +1):
+#         print(i, 'is i')
+#         print(n, 'is n')
+#         if n % i == 0:
+#             return False
+#     return True
 
-# Example usage
-numbers = [k for k in range(1, 11)]
-for number in numbers:
-    if is_prime(number):
-        print(f"{number} is a prime number.")
+# # Example usage
+# numbers = [k for k in range(1, 11)]
+# for number in numbers:
+#     if is_prime(number):
+#         print(f"{number} is a prime number.")
 
 
 # Thats All from 31st october
+
+
+llist = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+def binary(array,number):
+    for i in range(len(array)):
+        if array[i] == number:
+            return i
+        
+print()
