e70ce888ae5b85ee313215b448f0612c28fc03ff
Arif Nabi <arifnabi571@gmail.com>
2024-11-09 09:41:18 +0500

    one more file

---
+++ november9.py
@@ -0,0 +1,23 @@
+class Pearson:
+    def __init__(self,name,age):
+        self.__name = name
+        self.__age = age
+        
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self,name):
+        self.__name = name
+    
+    @name.deleter
+    def name(self):
+        del self.__name
+        
+ps = Pearson(3,4)
+print(ps.name)
+ps.name = "John"
+print(ps.name)
+del ps.name
+print(ps.name)
